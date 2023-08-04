95b8adf67a1c56021e0489c363e8264ec4c52d84
Danika Gupta <danika@gprof.com>
2023-08-04 14:25:13 -0700

    Initial code

--- AudioCheck/ContentView.swift
+++ AudioCheck/ContentView.swift
@@ -6,17 +6,30 @@
 //
 
 import SwiftUI
+import AVFoundation
 
 struct ContentView: View {
+    let synthesizer = AVSpeechSynthesizer()
     var body: some View {
         VStack {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
             Text("Hello, world!")
+            Button("Say something") {
+                say("Something")
+            }
         }
         .padding()
     }
+    
+    func say(_ string: String) {
+            let utterance = AVSpeechUtterance(string: string)
+            utterance.voice = AVSpeechSynthesisVoice(language: "en-US")
+            utterance.rate = 0.5
+            synthesizer.speak(utterance)
+        }
+    
 }
 
 struct ContentView_Previews: PreviewProvider {
@@ -24,3 +37,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
