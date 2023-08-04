6127842ec389699d4f3f14781c3b37cf9b88cd4a
Danika Gupta <danika@gprof.com>
2023-08-04 14:22:56 -0700

    Initial Commit

---
+++ AudioCheck/AudioCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioCheckApp.swift
+//  AudioCheck
+//
+//  Created by Amit Gupta on 8/4/23.
+//
+
+import SwiftUI
+
+@main
+struct AudioCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
